397fe56b83d0476886aff38a6d1f8bd066504050
Linus Behrbohm <linusbehrbohm@web.de>
2019-01-01 22:54:36 +0100

    Add handling functions for keyboard input

--- src/input.rs
+++ src/input.rs
@@ -7,9 +7,36 @@ pub fn init() {
 
 }
 
+fn handle_key_press(key: winit::ScanCode) -> winit::ControlFlow {
+    println!("Pressed {}", key);
+    winit::ControlFlow::Continue
+}
+
+fn handle_key_release(key: winit::ScanCode) -> winit::ControlFlow {
+    println!("Released {}", key);
+    winit::ControlFlow::Continue
+}
+
+fn handle_keyboard_input(input: winit::KeyboardInput) -> winit::ControlFlow {
+    match input {
+        winit::KeyboardInput {
+            scancode,
+            state: winit::ElementState::Pressed,
+            .. } => handle_key_press(scancode),
+        winit::KeyboardInput {
+            scancode,
+            state: winit::ElementState::Released,
+            .. } => handle_key_release(scancode)
+    }
+}
+
 fn handle_window_event(event: winit::WindowEvent) -> winit::ControlFlow {
     match event {
         winit::WindowEvent::CloseRequested => winit::ControlFlow::Break,
+        winit::WindowEvent::KeyboardInput {
+            input,
+            ..
+        } => handle_keyboard_input(input),
         _ => winit::ControlFlow::Continue
     }
 }
